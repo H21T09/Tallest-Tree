ed68f20c4dec7fdb81c2e82edb1314635667b3f8
H21T09 <hgtung2002@gmail.com>
2025-01-25 02:36:05 +0700

    Update 25/1

---
+++ Assets/_Assets/Scripts/AutoRotate.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotate : MonoBehaviour
+{
+    public float rotationspeed = 50f;
+
+    private void Update()
+    {
+        transform.Rotate(0,0,rotationspeed*Time.deltaTime);
+    }
+
+    
+}
