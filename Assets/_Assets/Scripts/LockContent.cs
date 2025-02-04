86db61d43f17e1d7a03b826d4dc9de6523b89ef2
H21T09 <hgtung2002@gmail.com>
2025-02-05 02:33:06 +0700

    Update 5/2 1

---
+++ Assets/_Assets/Scripts/LockContent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockContent : MonoBehaviour
+{
+    private RectTransform rect;
+    public float minY = 0f;
+    public float maxY = 3100f;
+    private void Start()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        
+        // Lấy giá trị Width hiện tại để giữ nguyên kích thước
+        float width = rect.rect.width;
+
+        // Gán giá trị Left và Right
+        rect.offsetMin = new Vector2(0, rect.offsetMin.y); // Giữ nguyên Left
+        rect.offsetMax = new Vector2(0, rect.offsetMax.y); // Giữ nguyên Right
+        
+
+        // Nếu cần, có thể khóa luôn scale và position
+        rect.localScale = Vector3.one;
+        rect.anchoredPosition = new Vector2(0, rect.anchoredPosition.y);
+
+
+        // Lấy vị trí hiện tại của content
+        Vector2 pos = rect.anchoredPosition;
+
+        // Giữ posY trong khoảng từ minY đến maxY
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        // Cập nhật lại vị trí
+        rect.anchoredPosition = pos;
+    }
+}
---
+++ Assets/_Assets/Scripts/PlayerBounce.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBounce : MonoBehaviour
+{
+    public float bounceForce = 5f; // Lực bật ngược lại
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            Vector2 bounceDirection = (transform.position - collision.transform.position).normalized;
+            rb.velocity = Vector2.zero; // Đặt vận tốc về 0 trước khi bật lại
+            rb.AddForce(bounceDirection * bounceForce, ForceMode2D.Impulse);
+        }
+    }
+}
